2ff98db84b227479d55241e742d8baf34787d9fd
Steinar Bang <sb@dod.no>
2016-08-30 23:01:29 +0200

    Removed unnecessary provider base class.
    The reason the provider didn't start and expose its service, was that it
    didn't get all of its required injections, not because the
    Jsr330Activator didn't recognize the class as a provider.

--- ukelonn.bundle.test.db/src/main/java/no/priv/bang/ukelonn/bundle/test/db/UkelonnDatabaseProvider.java
+++ ukelonn.bundle.test.db/src/main/java/no/priv/bang/ukelonn/bundle/test/db/UkelonnDatabaseProvider.java
@@ -16,7 +16,7 @@ import org.osgi.service.log.LogService;
 
 import no.priv.bang.ukelonn.UkelonnDatabase;
 
-public class UkelonnDatabaseProvider extends UkelonnDatabaseBase implements Provider<UkelonnDatabase> {
+public class UkelonnDatabaseProvider implements Provider<UkelonnDatabase>, UkelonnDatabase {
     private LogService logService;
     private Connection connect = null;
     private DataSourceFactory dataSourceFactory;
@@ -85,7 +85,6 @@ public class UkelonnDatabaseProvider extends UkelonnDatabaseBase implements Prov
         return insertedRowCounts;
     }
 
-    @Override
     public ResultSet query(String sqlQuery) {
         try {
             Statement statement = connect.createStatement();
