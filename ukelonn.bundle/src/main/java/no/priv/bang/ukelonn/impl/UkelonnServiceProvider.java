b893ae3567d036813dc96bdc83057ae8439d53be
Steinar Bang <sb@dod.no>
2016-09-01 21:57:07 +0200

    Removed unused JSP stuff.

--- ukelonn.bundle/src/main/java/no/priv/bang/ukelonn/impl/UkelonnServiceProvider.java
+++ ukelonn.bundle/src/main/java/no/priv/bang/ukelonn/impl/UkelonnServiceProvider.java
@@ -27,10 +27,6 @@ import no.steria.osgi.jsr330activator.Jsr330Activator;
  *
  */
 public class UkelonnServiceProvider extends UkelonnServiceBase implements Provider<UkelonnService> {
-    private static final String JSP = "/ukelonn/jsp";
-    private static final String JSPC = JSP + 'c';
-    private static final String DEFAULT_PACKAGE = "org.apache.jsp.";
-
     private WebContainer webContainer;
     private HttpContext httpContext;
     private UkelonnDatabase database;
@@ -49,35 +45,9 @@ public class UkelonnServiceProvider extends UkelonnServiceBase implements Provid
     public void setWebContainer(WebContainer webcontainer) throws ClassNotFoundException, ServletException, NamespaceException {
         webContainer = webcontainer;
         httpContext = webContainer.createDefaultHttpContext();
-        Bundle bundle = FrameworkUtil.getBundle(getClass());
-        Enumeration<?> entries = bundle.findEntries(JSP, "*", true);
-        if (entries != null) {
-            while (entries.hasMoreElements()) {
-                URL entry = (URL) entries.nextElement();
-                String jspFile = entry.toExternalForm().substring(entry.toExternalForm().lastIndexOf('/') + 1);
-                String urlPattern = JSPC + "/" + jspFile;
-                String jspcClassName = DEFAULT_PACKAGE
-                    + convertPath(entry.toExternalForm()) + "."
-                    + JspUtil.makeJavaIdentifier(jspFile);
-                @SuppressWarnings("unchecked")
-                    Class<Servlet> precompiledClass = (Class<Servlet>) getClass().getClassLoader().loadClass(jspcClassName);
-                webContainer.registerServlet(precompiledClass, new String[] { urlPattern }, null, httpContext);
-            }
-        }
-
-        webContainer.registerJsps(new String[] { JSP + "/*" }, httpContext);
 
         // register images as resources
         webContainer.registerResources("/images", "/images", httpContext);
-        // webContainer.end(httpContext);
-    }
-
-    private String convertPath(String jspPath) {
-        // String path = jspPath.replaceFirst("bundle\\:\\/\\/\\d*\\.\\d*.\\d\\/", "");
-        String path = jspPath.replaceFirst("(bundle.*://\\d*\\.)((\\w*)|(\\d*\\:\\d*))/", "");
-        path = path.substring(0, path.lastIndexOf('/'));
-        path = path.replace("/", ".");
-        return path;
     }
 
     public UkelonnService get() {
