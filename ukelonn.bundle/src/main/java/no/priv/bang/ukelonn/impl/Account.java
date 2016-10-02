f2099acd46f6a703b2619fff29727939d71ba867
Steinar Bang <sb@dod.no>
2016-10-02 20:05:30 +0200

    Compile fix: removed JSF bean converter registrations.

--- ukelonn.bundle/src/main/java/no/priv/bang/ukelonn/impl/Account.java
+++ ukelonn.bundle/src/main/java/no/priv/bang/ukelonn/impl/Account.java
@@ -19,8 +19,6 @@ public class Account {
         this.firstName = firstName;
         this.lastName = lastName;
         this.balance = balance;
-
-        AccountConverter.registerAccount(this);
     }
 
     public int getAccountId() {
--- ukelonn.bundle/src/main/java/no/priv/bang/ukelonn/impl/TransactionType.java
+++ ukelonn.bundle/src/main/java/no/priv/bang/ukelonn/impl/TransactionType.java
@@ -13,8 +13,6 @@ public class TransactionType {
         setTransactionAmount(transactionAmount);
         setTransactionIsWork(transactionIsWork);
         setTransactionIsWagePayment(transactionIsWagePayment);
-
-        TransactionTypeConverter.registerTransactionType(this);
     }
 
     @Override
