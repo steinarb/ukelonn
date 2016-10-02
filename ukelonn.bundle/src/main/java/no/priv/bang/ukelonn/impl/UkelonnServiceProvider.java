47845b3d5ab9c6efaeb6a3ffdb2684463ca596ea
Steinar Bang <sb@dod.no>
2016-10-02 20:04:17 +0200

    Added static provider instance accessor used by the static database methods.
    Will try to remove this later if possible.  Singletons are a bad pattern.

--- ukelonn.bundle/src/main/java/no/priv/bang/ukelonn/impl/UkelonnServiceProvider.java
+++ ukelonn.bundle/src/main/java/no/priv/bang/ukelonn/impl/UkelonnServiceProvider.java
@@ -25,9 +25,19 @@ import no.steria.osgi.jsr330activator.Jsr330Activator;
  */
 public class UkelonnServiceProvider extends UkelonnServiceBase implements Provider<UkelonnService> {
 
+    private static UkelonnService instance;
     private WebContainer webContainer;
     private HttpContext httpContext;
     private UkelonnDatabase database;
+
+    public static UkelonnService getInstance() {
+        return instance;
+    }
+
+    public UkelonnServiceProvider() {
+        super();
+        instance = this;
+    }
 
     @Inject
     public void setUkelonnDatabase(UkelonnDatabase database) {
