8ec20f95f3f9ef1c401b6967acb71b7058ab928c
Steinar Bang <sb@dod.no>
2016-10-03 18:11:08 +0200

    Added a PostgreSQL UkelonnDatabase OSGi service implementation.
    The Unit tests are rudimentary, and fragile.
    
    The Unit tests just test that the UkelonnDatabase implementation can
    connect to a local PostgreSQL database server that contains a database
    named "ukelonn" and that this database contains certain data.

---
+++ ukelonn.bundle.postgresql.db/src/main/java/no/priv/bang/ukelonn/bundle/postgresql/db/PGUkelonnDatabaseProvider.java
@@ -0,0 +1,80 @@
+package no.priv.bang.ukelonn.bundle.postgresql.db;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.sql.ConnectionPoolDataSource;
+import javax.sql.PooledConnection;
+
+import org.osgi.service.jdbc.DataSourceFactory;
+import org.osgi.service.log.LogService;
+
+import no.priv.bang.ukelonn.UkelonnDatabase;
+
+public class PGUkelonnDatabaseProvider implements Provider<UkelonnDatabase>, UkelonnDatabase {
+    private LogService logService;
+    private PooledConnection connect = null;
+    private DataSourceFactory dataSourceFactory;
+
+    @Inject
+    public void setLogService(LogService logService) {
+    	this.logService = logService;
+    }
+
+    @Inject
+    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
+    	this.dataSourceFactory = dataSourceFactory;
+    	if (dataSourceFactory != null) {
+            createConnection();
+    	}
+    }
+
+    void createConnection() {
+        Properties properties = new Properties();
+        properties.setProperty(DataSourceFactory.JDBC_URL, "jdbc:postgresql:///ukelonn");
+        try {
+            ConnectionPoolDataSource dataSource = dataSourceFactory.createConnectionPoolDataSource(properties);
+            connect = dataSource.getPooledConnection();
+        } catch (Exception e) {
+            logError("Derby mock database failed to create connection", e);
+        }
+    }
+
+    public UkelonnDatabase get() {
+        return this;
+    }
+
+    public ResultSet query(String sqlQuery) {
+        try {
+            Statement statement = connect.getConnection().createStatement();
+            ResultSet result = statement.executeQuery(sqlQuery);
+            return result;
+        } catch (Exception e) {
+            logError("PostgreSQL database query failed", e);
+        }
+
+        return null;
+    }
+
+    public int update(String sql) {
+        try {
+            Statement statement = connect.getConnection().createStatement();
+            int result = statement.executeUpdate(sql);
+            return result;
+        } catch (Exception e) {
+            logError("PostgreSQL database update failed", e);
+        }
+
+        return 0;
+    }
+
+    private void logError(String message, Exception exception) {
+        if (logService != null) {
+            logService.log(LogService.LOG_ERROR, message, exception);
+        }
+    }
+
+}
---
+++ ukelonn.bundle.postgresql.db/src/test/java/no/priv/bang/ukelonn/bundle/postgresql/db/PGUkelonnDatabaseProviderTest.java
@@ -0,0 +1,80 @@
+package no.priv.bang.ukelonn.bundle.postgresql.db;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.osgi.service.jdbc.DataSourceFactory;
+import org.postgresql.osgi.PGDataSourceFactory;
+
+import no.priv.bang.ukelonn.UkelonnDatabase;
+import no.priv.bang.ukelonn.bundle.postgresql.db.mocks.MockLogService;
+
+public class PGUkelonnDatabaseProviderTest {
+
+    @Test
+    public void testDatabase() throws SQLException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        PGUkelonnDatabaseProvider provider = new PGUkelonnDatabaseProvider();
+        provider.setLogService(new MockLogService());
+        DataSourceFactory dataSourceFactory = new PGDataSourceFactory();
+        setPrivateField(provider, "dataSourceFactory", dataSourceFactory); // Avoid side effects of the public setter
+        provider.createConnection();
+
+        // Test the database by making a query using a view
+        UkelonnDatabase database = provider.get();
+        ResultSet onAccount = database.query("select * from accounts_view where username='jad'");
+        assertNotNull(onAccount);
+        while (onAccount.next()) {
+            int account_id = onAccount.getInt("account_id");
+            int user_id = onAccount.getInt("user_id");
+            String username = onAccount.getString("username");
+            String first_name = onAccount.getString("first_name");
+            String last_name = onAccount.getString("last_name");
+            assertEquals(3, account_id);
+            assertEquals(3, user_id);
+            assertEquals("jad", username);
+            assertEquals("Jane", first_name);
+            assertEquals("Doe", last_name);
+        }
+    }
+
+    @Test
+    public void testAdministratorsView() throws SQLException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        PGUkelonnDatabaseProvider provider = new PGUkelonnDatabaseProvider();
+        provider.setLogService(new MockLogService());
+        DataSourceFactory dataSourceFactory = new PGDataSourceFactory();
+        provider.setDataSourceFactory(dataSourceFactory); // Simulate injection
+
+        UkelonnDatabase database = provider.get();
+
+        // Test that the database has users
+        ResultSet allUsers = database.query("select * from users");
+        int allUserCount = 0;
+        while (allUsers.next()) { ++allUserCount; }
+        assertThat(allUserCount, greaterThan(0));
+
+        // Test that the database administrators table has rows
+        ResultSet allAdministrators = database.query("select * from administrators");
+        int allAdminstratorsCount = 0;
+        while (allAdministrators.next()) { ++allAdminstratorsCount; }
+        assertThat(allAdminstratorsCount, greaterThan(0));
+
+        // Test that the administrators_view is present
+        ResultSet allAdministratorsView = database.query("select * from administrators_view");
+        int allAdminstratorsViewCount = 0;
+        while (allAdministratorsView.next()) { ++allAdminstratorsViewCount; }
+        assertEquals(2, allAdminstratorsViewCount);
+    }
+
+    private void setPrivateField(Object object, String fieldName, Object value) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        Field field = object.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(object, value);
+    }
+
+}
