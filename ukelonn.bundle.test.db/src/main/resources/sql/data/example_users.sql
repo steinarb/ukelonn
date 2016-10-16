80626d4d60ff96f85f8c420b9ffa86a2163a7e9c
Steinar Bang <sb@dod.no>
2016-10-17 00:18:52 +0200

    Added hashed passwords and hash salt to the sample users of the derby test database.
    The hashed passwords and the salt used in this file, was generated by
    the unit test UkelonnDatabaseProviderTest.testCreateHashedPasswords().

--- ukelonn.bundle.test.db/src/main/resources/sql/data/example_users.sql
+++ ukelonn.bundle.test.db/src/main/resources/sql/data/example_users.sql
@@ -1,4 +1,4 @@
-insert into users (username,password,email,first_name,last_name) values ('on','ola12','olanordmann2345@gmail.com','Ola','Nordmann');
-insert into users (username,password,email,first_name,last_name) values ('kn','KaRi','karinordmann3456@gmail.com','Kari','Nordmann');
-insert into users (username,password,email,first_name,last_name) values ('jad','1ad','janedoe7896@gmail.com','Jane','Doe');
-insert into users (username,password,email,first_name,last_name) values ('jod','johnnyBoi','johndoe6789@gmail.com','John','Doe');
+insert into users (username,password,salt,email,first_name,last_name) values ('on','6VuUrsvVkZfxtKwt4tdCmOdXtXCuIbgWhcURzeRpT/g=', 'snKBcs4FMoGZHQlNY2kz5w==','olanordmann2345@gmail.com','Ola','Nordmann');
+insert into users (username,password,salt,email,first_name,last_name) values ('kn','560/eDyvqQcf8c98w8NEg4W26u5aYzLPlnpaNNTJxYM=', 'pUQ1F2Yjn+kLb/JzOPDG4A==','karinordmann3456@gmail.com','Kari','Nordmann');
+insert into users (username,password,salt,email,first_name,last_name) values ('jad','adEp0s7cQHrJcIsDTDoFQK8eUAIFGh23wew7Klis1sk=', '78dCvMDECA47YMdtCkgkwQ==','janedoe7896@gmail.com','Jane','Doe');
+insert into users (username,password,salt,email,first_name,last_name) values ('jod','sU4vKCNpoS6AuWAzZhkNk7BdXSNkW2tmOP53nfotDjE=', '9SFDvohxZkZ9eWHiSEoMDw==','johndoe6789@gmail.com','John','Doe');
