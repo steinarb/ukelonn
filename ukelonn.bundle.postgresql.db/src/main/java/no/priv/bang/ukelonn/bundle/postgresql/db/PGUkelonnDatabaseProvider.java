d5c7bd243b39a470c0c619dedeb2c92cae918c5f
Steinar Bang <sb@dod.no>
2016-11-12 12:12:37 +0100

    Fixed copy/paste error in log message.

--- ukelonn.bundle.postgresql.db/src/main/java/no/priv/bang/ukelonn/bundle/postgresql/db/PGUkelonnDatabaseProvider.java
+++ ukelonn.bundle.postgresql.db/src/main/java/no/priv/bang/ukelonn/bundle/postgresql/db/PGUkelonnDatabaseProvider.java
@@ -39,7 +39,7 @@ public class PGUkelonnDatabaseProvider implements Provider<UkelonnDatabase>, Uke
             ConnectionPoolDataSource dataSource = dataSourceFactory.createConnectionPoolDataSource(properties);
             connect = dataSource.getPooledConnection();
         } catch (Exception e) {
-            logError("Derby mock database failed to create connection", e);
+            logError("Failed to create connection to PostgreSQL database", e);
         }
     }
 
