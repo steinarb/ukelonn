c21c1c1730aa654fabdaec6b9caf7fb7f2061bff
Steinar Bang <sb@dod.no>
2016-12-03 16:17:29 +0100

    Simpler access to the username: use the getUserPrincipal() method of the VaadinRequest argument to the init() method.

--- ukelonn.bundle/src/main/java/no/priv/bang/ukelonn/impl/UkelonnUI.java
+++ ukelonn.bundle/src/main/java/no/priv/bang/ukelonn/impl/UkelonnUI.java
@@ -1,12 +1,13 @@
 package no.priv.bang.ukelonn.impl;
 
 import static no.priv.bang.ukelonn.impl.CommonDatabaseMethods.*;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
+
+import java.security.Principal;
 
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
@@ -17,17 +18,19 @@ public class UkelonnUI extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
-    	Subject currentUser = SecurityUtils.getSubject();
-    	Account account = getAccountInfoFromDatabase(getClass(), (String) currentUser.getPrincipal());
+    	Principal currentUser = request.getUserPrincipal();
+    	Account account = getAccountInfoFromDatabase(getClass(), (String) currentUser.getName());
         // Create the content root layout for the UI
         VerticalLayout content = new VerticalLayout();
         setContent(content);
 
         // Display the greeting
-        content.addComponent(new Label("Hello " + account.getFirstName()));
+        Component greeting = new Label("Hello " + account.getFirstName());
+        greeting.setStyleName("h1");
+        content.addComponent(greeting);
 
         // Have a clickable button
-        content.addComponent(new Button("Push Me!",
+        content.addComponent(new Button("Registrer jobb",
                                         new Button.ClickListener() {
                                             private static final long serialVersionUID = 2723190031041985566L;
 
