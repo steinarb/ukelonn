31195a936e754be07f38cb15af76d3983a3e293a
Steinar Bang <sb@dod.no>
2017-05-24 00:06:54 +0200

    Remove unused SQL files.
    The schema is now set up using liquibase changesets for both derby and
    PostgreSQL.
    
    The only SQL files left, are the SQL files that sets up mock data for
    the derby database.  These files have been changed to become liquibase
    changeset compliant.

--- ukelonn.bundle.db.test/src/main/resources/sql/views/administrators_view.sql
+++
@@ -1,5 +0,0 @@
-create view administrators_view (administrator_id, user_id, username, first_name, last_name)
-as select administrators.administrator_id, users.user_id, username, first_name, last_name
- from users
- join administrators on administrators.user_id=users.user_id
-group by administrators.administrator_id, users.user_id, username, first_name, last_name
