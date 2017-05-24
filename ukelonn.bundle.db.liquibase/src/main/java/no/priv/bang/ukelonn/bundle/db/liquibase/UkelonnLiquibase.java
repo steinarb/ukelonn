f0b1a15ddb86d314e7b0979b0e40c53d03cbf7dc
Steinar Bang <sb@dod.no>
2017-05-24 23:21:12 +0200

    Remove the UkelonnLiquibase.readSchema() method.
    The method was used for the initial creation of a liquibase changelog,
    and is no longer needed.

--- ukelonn.bundle.db.liquibase/src/main/java/no/priv/bang/ukelonn/bundle/db/liquibase/UkelonnLiquibase.java
+++ ukelonn.bundle.db.liquibase/src/main/java/no/priv/bang/ukelonn/bundle/db/liquibase/UkelonnLiquibase.java
@@ -4,35 +4,12 @@ import java.sql.SQLException;
 import javax.sql.PooledConnection;
 
 import liquibase.Liquibase;
-import liquibase.command.GenerateChangeLogCommand;
-import liquibase.database.Database;
 import liquibase.database.DatabaseConnection;
 import liquibase.database.jvm.JdbcConnection;
-import liquibase.diff.compare.CompareControl;
-import liquibase.diff.output.DiffOutputControl;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
 
 public class UkelonnLiquibase {
-
-    public void readSchema(PooledConnection connect) {
-        try {
-            DatabaseConnection databaseConnection = new JdbcConnection(connect.getConnection());
-            Liquibase liquibase = new Liquibase(null, null, databaseConnection);
-            Database database = liquibase.getDatabase();
-            DiffOutputControl diffOutputControl = new DiffOutputControl();
-            CompareControl compareControl = new CompareControl();
-            GenerateChangeLogCommand command = new GenerateChangeLogCommand();
-            command.setReferenceDatabase(database)
-                .setOutputStream(System.out)
-                .setCompareControl(compareControl);
-            command.setDiffOutputControl(diffOutputControl);
-
-            command.execute();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public void createSchema(PooledConnection connect) throws SQLException, LiquibaseException {
         DatabaseConnection databaseConnection = new JdbcConnection(connect.getConnection());
