c55d402d1815f84238b2fdfc198f3d2c2a9609c6
Steinar Bang <sb@dod.no>
2017-05-26 17:39:36 +0200

    Make database unit test fail on empty non-null resultset.

--- ukelonn.bundle.db.test/src/test/java/no/priv/bang/ukelonn/bundle/db/test/UkelonnDatabaseProviderTest.java
+++ ukelonn.bundle.db.test/src/test/java/no/priv/bang/ukelonn/bundle/db/test/UkelonnDatabaseProviderTest.java
@@ -53,18 +53,17 @@ public class UkelonnDatabaseProviderTest {
         UkelonnDatabase database = provider.get();
         ResultSet onAccount = database.query("select * from accounts_view where username='jad'");
         assertNotNull(onAccount);
-        while (onAccount.next()) {
-            int account_id = onAccount.getInt("account_id");
-            int user_id = onAccount.getInt("user_id");
-            String username = onAccount.getString("username");
-            String first_name = onAccount.getString("first_name");
-            String last_name = onAccount.getString("last_name");
-            assertEquals(3, account_id);
-            assertEquals(3, user_id);
-            assertEquals("jad", username);
-            assertEquals("Jane", first_name);
-            assertEquals("Doe", last_name);
-        }
+        assertTrue(onAccount.next());
+        int account_id = onAccount.getInt("account_id");
+        int user_id = onAccount.getInt("user_id");
+        String username = onAccount.getString("username");
+        String first_name = onAccount.getString("first_name");
+        String last_name = onAccount.getString("last_name");
+        assertEquals(3, account_id);
+        assertEquals(3, user_id);
+        assertEquals("jad", username);
+        assertEquals("Jane", first_name);
+        assertEquals("Doe", last_name);
 
         // Verify that the schema changeset as well as all of the test data change sets has been run
         List<RanChangeSet> ranChangeSets = provider.getChangeLogHistory();
