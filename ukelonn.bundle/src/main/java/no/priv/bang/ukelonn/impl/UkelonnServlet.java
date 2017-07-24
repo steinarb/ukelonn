d861de9c9c6ca001826c59eebe13f664f060a0b0
Steinar Bang <sb@dod.no>
2017-07-25 00:59:09 +0200

    Fix formatting, change tabs to spaces.

--- ukelonn.bundle/src/main/java/no/priv/bang/ukelonn/impl/UkelonnServlet.java
+++ ukelonn.bundle/src/main/java/no/priv/bang/ukelonn/impl/UkelonnServlet.java
@@ -31,9 +31,9 @@ public class UkelonnServlet extends TouchKitServlet {
     private void addSessionInitListenerThatWillSetUIProviderOnSession() {
         VaadinServletService service = getService();
         service.addSessionInitListener(new SessionInitListener() {
-			private static final long serialVersionUID = -5085594781477821868L;
+                private static final long serialVersionUID = -5085594781477821868L;
 
-				@Override
+                @Override
                 public void sessionInit(SessionInitEvent sessionInitEvent) throws ServiceException {
                     VaadinSession session = sessionInitEvent.getSession();
                     removeDefaultUIProvider(session);
@@ -41,12 +41,12 @@ public class UkelonnServlet extends TouchKitServlet {
                 }
 
                 private void removeDefaultUIProvider(VaadinSession session) {
-                	List<UIProvider> uiProviders = new ArrayList<UIProvider>(session.getUIProviders());
-                	for (UIProvider uiProvider : uiProviders) {
-						if (DefaultUIProvider.class.getCanonicalName().equals(uiProvider.getClass().getCanonicalName())) {
-							session.removeUIProvider(uiProvider);
-						}
-					}
+                    List<UIProvider> uiProviders = new ArrayList<UIProvider>(session.getUIProviders());
+                    for (UIProvider uiProvider : uiProviders) {
+                        if (DefaultUIProvider.class.getCanonicalName().equals(uiProvider.getClass().getCanonicalName())) {
+                            session.removeUIProvider(uiProvider);
+                        }
+                    }
                 }
             });
     }
