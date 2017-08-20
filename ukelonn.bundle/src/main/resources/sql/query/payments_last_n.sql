d0ecc4488837296233316397c4da3c9794a6b9af
Steinar Bang <sb@dod.no>
2017-08-20 15:51:19 +0200

    Add dummy "paid_out" column to the payments SQL query.

--- ukelonn.bundle/src/main/resources/sql/query/payments_last_n.sql
+++ ukelonn.bundle/src/main/resources/sql/query/payments_last_n.sql
@@ -1,7 +1,7 @@
-select * from transactions
-  join transaction_types on transaction_types.transaction_type_id=transactions.transaction_type_id
- where transaction_id in
+select t.*, tt.*, false as paid_out from transactions t
+  join transaction_types tt on tt.transaction_type_id=t.transaction_type_id
+ where t.transaction_id in
  (select transaction_id from transactions
   join transaction_types on transaction_types.transaction_type_id=transactions.transaction_type_id
   where account_id=? and transaction_types.transaction_is_wage_payment order by transaction_id desc fetch next %d rows only)
- order by transaction_id
+ order by t.transaction_id
