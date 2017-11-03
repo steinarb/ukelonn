acb0e3aa71e899b8a27daac20729d9dd0d2f967a
Steinar Bang <sb@dod.no>
2017-11-03 22:19:12 +0100

    Sonar fix: Remove Thread.sleep() from test.
    When the system moves to using the whiteboard extender, the test will
    wait for a servlet injection, and there will be no need for a
    Thread.sleep() (and the test may actually work).

--- ukelonn.tests/src/test/java/no/priv/bang/ukelonn/tests/UkelonnServiceIntegrationTest.java
+++ ukelonn.tests/src/test/java/no/priv/bang/ukelonn/tests/UkelonnServiceIntegrationTest.java
@@ -109,7 +109,6 @@ public class UkelonnServiceIntegrationTest extends UkelonnServiceIntegrationTest
     @Ignore("I haven't been able to make this one work yet but hope I will.")
     @Test
     public void webappAccessTest() throws Exception {
-        Thread.sleep(20*1000);
         HttpTestClient testclient = HttpTestClientFactory.createDefaultTestClient();
         try {
             testclient.doGET("http://localhost:8081/ukelonn/").withReturnCode(404);
