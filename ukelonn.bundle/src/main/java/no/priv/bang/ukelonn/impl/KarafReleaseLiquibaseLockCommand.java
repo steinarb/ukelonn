953066f769fbb0904f1a029b01ea53ff2009a9fc
Steinar Bang <sb@dod.no>
2018-06-27 20:45:34 +0200

    Stop Sonar requiring a logger for console command output

--- ukelonn.bundle/src/main/java/no/priv/bang/ukelonn/impl/KarafReleaseLiquibaseLockCommand.java
+++ ukelonn.bundle/src/main/java/no/priv/bang/ukelonn/impl/KarafReleaseLiquibaseLockCommand.java
@@ -31,7 +31,7 @@ public class KarafReleaseLiquibaseLockCommand implements Action {
     @Override
     public Object execute() throws Exception {
         database.forceReleaseLocks();
-        System.out.println("Forcibly unlocked the Liquibase changelog lock");
+        System.out.println("Forcibly unlocked the Liquibase changelog lock"); // NOSONAR This is command output and should not go to a logger
         return null;
     }
 }
