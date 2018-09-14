2dbdc9fdf16e31c3ec4bfad02614ff98be7f307e
Steinar Bang <sb@dod.no>
2018-09-14 23:57:30 +0200

    Style jobs delete page with material design lite

--- ukelonn.bundle/src/main/frontend/components/AdminJobsDelete.js
+++ ukelonn.bundle/src/main/frontend/components/AdminJobsDelete.js
@@ -33,41 +33,58 @@ class AdminJobsDelete extends Component {
             return <Redirect to="/ukelonn/login" />;
         }
 
+        const reduceHeaderRowPadding = { padding: '0 0 0 0' };
+
         return (
-            <div>
-                <Link to="/ukelonn/admin/jobtypes">Administer jobber og jobbtyper</Link>
-                <br/>
-                <h1>Slett feilregisterte jobber for {account.firstName}</h1>
+            <div className="mdl-layout mdl-layout--fixed-header">
+                <header className="mdl-layout__header">
+                    <div className="mdl-layout__header-row" style={reduceHeaderRowPadding}>
+                        <Link to="/ukelonn/admin/jobtypes" className="mdl-navigation__link">
+                            <i className="material-icons" >chevron_left</i>
+                            &nbsp;
+                            Administrer jobber og jobbtyper
+                        </Link>
+                        <span className="mdl-layout-title">Slett jobber</span>
+                    </div>
+                </header>
+                <main className="mdl-layout__content">
 
-                <p><em>Merk!</em> Det er bare feilregistreringer som skal slettes!<br/>
-                   <em>Ikke</em> slett jobber som skal utbetales</p>
-                <label htmlFor="account-selector">Velg konto:</label>
-                <Accounts  id="account-selector" accounts={accounts} accountsMap={accountsMap} account={account} onAccountsFieldChange={onAccountsFieldChange}/>
+                    <p><em>Merk!</em> Det er bare feilregistreringer som skal slettes!<br/>
+                        <em>Ikke</em> slett jobber som skal utbetales</p>
 
-                <table className="table table-bordered">
-                    <thead>
-                        <tr>
-                            <td>Slett</td>
-                            <td>Dato</td>
-                            <td>Jobber</td>
-                            <td>Beløp</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {jobs.map((job) =>
-                            <tr key={job.id}>
-                                <td><input type="checkbox" checked={job.delete} onChange={(e) => onCheckboxTicked(e.target.checked, job, jobs)}/></td>
-                                <td>{job.transactionTime}</td>
-                                <td>{job.name}</td>
-                                <td>{job.transactionAmount}</td>
+                    <div className="mdl-grid hline-bottom">
+                        <div className="mdl-cell mdl-cell--2-col-phone mdl-cell--3-col-tablet mdl-cell--3-col-desktop">
+                            <label htmlFor="account-selector">Velg konto:</label>
+                        </div>
+                        <div className="mdl-cell mdl-cell--2-col-phone mdl-cell--5-col-tablet mdl-cell--9-col-desktop">
+                            <Accounts  id="account-selector" accounts={accounts} accountsMap={accountsMap} account={account} onAccountsFieldChange={onAccountsFieldChange}/>
+                        </div>
+                    </div>
+                    <table className="mdl-data-table mdl-js-data-table transaction-table">
+                        <thead>
+                            <tr>
+                                <td className="mdl-data-table__cell--non-numeric transaction-table-col transaction-table-col4">Slett</td>
+                                <td className="mdl-data-table__cell--non-numeric transaction-table-col transaction-table-col1">Dato</td>
+                                <td className="mdl-data-table__cell--non-numeric transaction-table-col transaction-table-col-hide-overflow transaction-table-col2">Jobber</td>
+                                <td className="transaction-table-col transaction-table-col3">Bel.</td>
                             </tr>
-                        )}
-                    </tbody>
-                </table>
-                <button onClick={() => onDeleteMarkedJobs(account, jobs)}>Slett merkede jobber</button>
+                        </thead>
+                        <tbody>
+                            {jobs.map((job) =>
+                                <tr key={job.id}>
+                                    <td className="mdl-data-table__cell--non-numeric transaction-table-col"><input type="checkbox" checked={job.delete} onChange={(e) => onCheckboxTicked(e.target.checked, job, jobs)}/></td>
+                                    <td className="mdl-data-table__cell--non-numeric transaction-table-col">{job.transactionTime}</td>
+                                    <td className="mdl-data-table__cell--non-numeric transaction-table-col transaction-table-col-hide-overflow">{job.name}</td>
+                                    <td className="transaction-table-col">{job.transactionAmount}</td>
+                                 </tr>
+                            )}
+                        </tbody>
+                    </table>
+                    <button className="mdl-button mdl-js-button mdl-button--raised" onClick={() => onDeleteMarkedJobs(account, jobs)}>Slett merkede jobber</button>
+                </main>
                 <br/>
                 <br/>
-                <button onClick={() => onLogout()}>Logout</button>
+                <button className="mdl-button mdl-js-button mdl-button--raised" onClick={() => onLogout()}>Logout</button>
             </div>
         );
     }
