0ee9c1dbc2255bbb307041b16bf4582c4a34c834
Steinar Bang <sb@dod.no>
2018-10-04 21:50:42 +0200

    Style up job edit page with material design lite

--- ukelonn.web.frontend/src/main/frontend/components/AdminJobsEdit.js
+++ ukelonn.web.frontend/src/main/frontend/components/AdminJobsEdit.js
@@ -38,45 +38,80 @@ class AdminJobsEdit extends Component {
             return <Redirect to="/ukelonn/login" />;
         }
 
+        const reduceHeaderRowPadding = { padding: '0 0 0 0' };
+
         return (
-            <div>
-                <Link to="/ukelonn/admin/jobtypes">Administer jobber og jobbtyper</Link>
-                <br/>
-                <h1>Endre jobber for {account.firstName}</h1>
+            <div className="mdl-layout mdl-layout--fixed-header">
+                <header className="mdl-layout__header">
+                    <div className="mdl-layout__header-row" style={reduceHeaderRowPadding}>
+                        <Link to="/ukelonn/admin/jobtypes" className="mdl-navigation__link">
+                            <i className="material-icons" >chevron_left</i>
+                            &nbsp;
+                            Administrer jobber og jobbtyper
+                        </Link>
+                        <span className="mdl-layout-title">Endre jobber for {account.firstName}</span>
+                    </div>
+                </header>
+                <main className="mdl-layout__content">
 
-                <label htmlFor="account-selector">Velg konto:</label>
-                <Accounts  id="account-selector" accounts={accounts} accountsMap={accountsMap} account={account} onAccountsFieldChange={onAccountsFieldChange}/>
-                <br/>
+                    <div className="mdl-grid hline-bottom">
+                        <div className="mdl-cell mdl-cell--2-col-phone mdl-cell--3-col-tablet mdl-cell--3-col-desktop">
+                            <label htmlFor="account-selector">Velg konto:</label>
+                        </div>
+                        <div className="mdl-cell mdl-cell--2-col-phone mdl-cell--5-col-tablet mdl-cell--9-col-desktop">
+                            <Accounts  id="account-selector" accounts={accounts} accountsMap={accountsMap} account={account} onAccountsFieldChange={onAccountsFieldChange}/>
+                        </div>
+                    </div>
 
-                <table className="table table-bordered">
-                    <thead>
-                        <tr>
-                            <td>Dato</td>
-                            <td>Jobber</td>
-                            <td>Beløp</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {jobs.map((job) =>
-                            <tr onClick={ ()=>onRowClick(account, job) } key={job.id}>
-                                <td>{moment(job.transactionTime).format("YYYY-MM-DD")}</td>
-                                <td>{job.name}</td>
-                                <td>{job.transactionAmount}</td>
+                    <table className="mdl-data-table mdl-js-data-table transaction-table">
+                        <thead>
+                            <tr>
+                                <td className="mdl-data-table__cell--non-numeric transaction-table-col transaction-table-col1">Dato</td>
+                                <td className="mdl-data-table__cell--non-numeric transaction-table-col transaction-table-col-hide-overflow transaction-table-col2">Jobber</td>
+                                <td className="transaction-table-col transaction-table-col3">Bel.</td>
                             </tr>
-                        )}
-                    </tbody>
-                </table>
-                <h2>Endre jobb</h2>
-                <label htmlFor="jobtype">Jobbtype</label>
-                <Jobtypes id="jobtype" jobtypes={jobtypes} jobtypesMap={jobtypesMap} value={selectedjob.transactionType.transactionTypeName} account={account} performedjob={selectedjob} onJobtypeFieldChange={onJobtypeFieldChange} />
-                <br/>
-                <label htmlFor="amount">Beløp</label>
-                <input id="amount" type="text" value={selectedjob.transactionAmount} readOnly="true" />
-                <br/>
-                <label htmlFor="date">Dato</label>
-                <DatePicker selected={selectedjob.transactionTime} dateFormat="YYYY-MM-DD" onChange={(selectedValue) => onDateFieldChange(selectedValue, selectedjob)} readOnly={true} />
-                <br/>
-                <button onClick={() => onSaveEditedJob(selectedjob)}>Lagre endret jobb</button>
+                        </thead>
+                        <tbody>
+                            {jobs.map((job) =>
+                                <tr onClick={ ()=>onRowClick(account, job) } key={job.id}>
+                                    <td className="mdl-data-table__cell--non-numeric transaction-table-col">{moment(job.transactionTime).format("YYYY-MM-DD")}</td>
+                                    <td className="mdl-data-table__cell--non-numeric transaction-table-col transaction-table-col-hide-overflow">{job.name}</td>
+                                    <td className="transaction-table-col">{job.transactionAmount}</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+
+                    <h2>Endre jobb</h2>
+
+                    <div className="mdl-grid hline-bottom">
+                        <div className="mdl-cell mdl-cell--2-col-phone mdl-cell--3-col-tablet mdl-cell--3-col-desktop">
+                            <label htmlFor="jobtype">Jobbtype</label>
+                        </div>
+                        <div className="mdl-cell mdl-cell--2-col-phone mdl-cell--5-col-tablet mdl-cell--9-col-desktop">
+                            <Jobtypes id="jobtype" jobtypes={jobtypes} jobtypesMap={jobtypesMap} value={selectedjob.transactionType.transactionTypeName} account={account} performedjob={selectedjob} onJobtypeFieldChange={onJobtypeFieldChange} />
+                        </div>
+                    </div>
+
+                    <div className="mdl-grid hline-bottom">
+                        <div className="mdl-cell mdl-cell--2-col-phone mdl-cell--3-col-tablet mdl-cell--3-col-desktop">
+                            <label htmlFor="amount">Beløp</label>
+                         </div>
+                         <div className="mdl-cell mdl-cell--2-col-phone mdl-cell--5-col-tablet mdl-cell--9-col-desktop">
+                            <input id="amount" type="text" value={selectedjob.transactionAmount} readOnly="true" />
+                         </div>
+                    </div>
+
+                    <div className="mdl-grid hline-bottom">
+                        <div className="mdl-cell mdl-cell--2-col-phone mdl-cell--3-col-tablet mdl-cell--3-col-desktop">
+                            <label htmlFor="date">Dato</label>
+                        </div>
+                        <div className="mdl-cell mdl-cell--2-col-phone mdl-cell--5-col-tablet mdl-cell--9-col-desktop">
+                            <DatePicker selected={selectedjob.transactionTime} dateFormat="YYYY-MM-DD" onChange={(selectedValue) => onDateFieldChange(selectedValue, selectedjob)} readOnly={true} />
+                        </div>
+                    </div>
+                    <button onClick={() => onSaveEditedJob(selectedjob)}>Lagre endret jobb</button>
+                </main>
                 <br/>
                 <br/>
                 <button onClick={() => onLogout()}>Logout</button>
