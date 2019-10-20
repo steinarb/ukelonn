0127a21b818523c05e6888faf3f4250fe1780520
Steinar Bang <sb@dod.no>
2019-10-20 20:18:26 +0200

    Make functional react components more like stateful components

--- ukelonn.web.frontend/src/main/frontend/components/Paymenttypes.js
+++ ukelonn.web.frontend/src/main/frontend/components/Paymenttypes.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Paymenttypes = ({id, className, paymenttypes, paymenttypesMap, value, account, paymenttype, onPaymenttypeFieldChange }) => (
-    <select id={id} className={className} onChange={(event) => onPaymenttypeFieldChange(event.target.value, paymenttypesMap, account)} value={value}>
-        {paymenttypes.map((val) => <option key={val.id}>{val.transactionTypeName}</option>)}
-    </select>
-);
+function Paymenttypes(props) {
+    const {id, className, paymenttypes, paymenttypesMap, value, account, paymenttype, onPaymenttypeFieldChange } = props;
+    return (
+        <select id={id} className={className} onChange={(event) => onPaymenttypeFieldChange(event.target.value, paymenttypesMap, account)} value={value}>
+          {paymenttypes.map((val) => <option key={val.id}>{val.transactionTypeName}</option>)}
+        </select>
+    );
+}
 
 export default Paymenttypes;
