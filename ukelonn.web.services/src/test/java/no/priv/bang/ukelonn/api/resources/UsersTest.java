68b2cf7d223d364707ca559af1dbe00a406cbe5f
Steinar Bang <sb@dod.no>
2019-11-10 07:58:14 +0100

    Adapt to authservice using jdbc-config instead of AuthserviceDatabaseService

--- ukelonn.web.services/src/test/java/no/priv/bang/ukelonn/api/resources/UsersTest.java
+++ ukelonn.web.services/src/test/java/no/priv/bang/ukelonn/api/resources/UsersTest.java
@@ -26,10 +26,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.sql.DataSource;
 import javax.ws.rs.InternalServerErrorException;
 
 import org.junit.Test;
-import no.priv.bang.authservice.definitions.AuthserviceDatabaseService;
 import no.priv.bang.authservice.users.UserManagementServiceProvider;
 import no.priv.bang.osgi.service.mocks.logservice.MockLogService;
 import no.priv.bang.osgiservice.users.User;
@@ -64,12 +64,12 @@ public class UsersTest {
         PreparedStatement statement = mock(PreparedStatement.class);
         ResultSet results = mock(ResultSet.class);
         when(results.next()).thenThrow(SQLException.class);
-        AuthserviceDatabaseService database = mock(AuthserviceDatabaseService.class);
+        DataSource datasource = mock(DataSource.class);
         Connection connection = mock(Connection.class);
-        when(database.getConnection()).thenReturn(connection);
+        when(datasource.getConnection()).thenReturn(connection);
         when(connection.prepareStatement(anyString())).thenReturn(statement);
         when(statement.executeQuery()).thenReturn(results);
-        useradmin.setDatabase(database);
+        useradmin.setDataSource(datasource);
 
         // Inject the UkelonnService
         resource.useradmin = useradmin;
