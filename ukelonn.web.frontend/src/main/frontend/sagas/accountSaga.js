bc980d204b1c4aa5d8006b767f1abe7627c70f07
Steinar Bang <sb@dod.no>
2020-03-14 09:59:50 +0100

    Load aggregate earnings when selecting account in admin

--- ukelonn.web.frontend/src/main/frontend/sagas/accountSaga.js
+++ ukelonn.web.frontend/src/main/frontend/sagas/accountSaga.js
@@ -1,6 +1,7 @@
 import { takeLatest, call, put, fork } from 'redux-saga/effects';
 import axios from 'axios';
 import {
+    UPDATE_ACCOUNT,
     ACCOUNT_REQUEST,
     ACCOUNT_RECEIVE,
     ACCOUNT_FAILURE,
@@ -14,6 +15,14 @@ import { emptyAccount } from '../constants';
 
 function doAccount(username) {
     return axios.get('/ukelonn/api/account/' + username );
+}
+
+// worker saga
+function* updateAccountSaga(action) {
+    const payload = action.payload || {};
+    const { username } = payload;
+    yield put(EARNINGS_SUM_OVER_YEAR_REQUEST(username));
+    yield put(EARNINGS_SUM_OVER_MONTH_REQUEST(username));
 }
 
 // worker saga
@@ -38,5 +47,6 @@ function* receiveAccountSaga(action) {
 
 // watcher saga
 export default function* accountSaga() {
+    yield takeLatest(UPDATE_ACCOUNT, updateAccountSaga);
     yield takeLatest(ACCOUNT_REQUEST, receiveAccountSaga);
 }
