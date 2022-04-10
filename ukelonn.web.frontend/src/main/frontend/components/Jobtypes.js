9ca637bfa55d2e22da0c6c9201699f492bb30204
Steinar Bang <sb@dod.no>
2022-04-10 17:13:37 +0200

    Fix indentation

--- ukelonn.web.frontend/src/main/frontend/components/Jobtypes.js
+++ ukelonn.web.frontend/src/main/frontend/components/Jobtypes.js
@@ -4,7 +4,7 @@ function Jobtypes(props) {
     const {id, className, value, jobtypes, onJobtypeFieldChange } = props;
     return (
         <select id={id} className={className} onChange={(event) => onJobtypeFieldChange(event.target.value, jobtypes)} value={value}>
-          {jobtypes.map((val) => <option key={val.id} value={val.id}>{val.transactionTypeName}</option>)}
+            {jobtypes.map((val) => <option key={val.id} value={val.id}>{val.transactionTypeName}</option>)}
         </select>
     );
 }
