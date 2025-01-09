3a421264b25fe55806c0c7fada3726e88d5a6441
Steinar Bang <sb@dod.no>
2025-01-09 18:43:29 +0100

    Use object with id=-1 as SELECTED_JOB_TYPES payload on job type unselect

--- ukelonn.web.frontend/src/main/frontend/components/Jobtypes.js
+++ ukelonn.web.frontend/src/main/frontend/components/Jobtypes.js
@@ -8,7 +8,7 @@ export default function Jobtypes(props) {
     const transactionTypeId = useSelector(state => state.transactionTypeId);
     const { data: jobtypes = [] } = useGetJobtypesQuery()
     const dispatch = useDispatch();
-    const onJobTypeSelected = e => dispatch(SELECTED_JOB_TYPE(jobtypes.find(t => t.id === parseInt(e.target.value))));
+    const onJobTypeSelected = e => dispatch(SELECTED_JOB_TYPE(jobtypes.find(t => t.id === parseInt(e.target.value)) || { id: -1 }));
 
     return (
         <select id={id} className={className} onChange={onJobTypeSelected} value={transactionTypeId}>
