962bd0609bbb0b954b0371300b622782d3444c31
Steinar Bang <sb@dod.no>
2025-01-30 22:23:01 +0100

    Remove leftover unused redux actions

--- ukelonn.web.frontend/src/main/frontend/actiontypes.js
+++ ukelonn.web.frontend/src/main/frontend/actiontypes.js
@@ -3,13 +3,8 @@ import { createAction } from '@reduxjs/toolkit';
 export const UPDATE_FIRSTTIMEAFTERLOGIN = createAction('UPDATE_FIRSTTIMEAFTERLOGIN');
 export const MODIFY_USER_IS_ADMINISTRATOR = createAction('MODIFY_USER_IS_ADMINISTRATOR');
 export const MODIFY_JOB_DATE = createAction('MODIFY_JOB_DATE');
-export const SAVE_CHANGES_TO_JOB_TYPE_BUTTON_CLICKED = createAction('SAVE_CHANGES_TO_JOB_TYPE_BUTTON_CLICKED');
 export const JOB_TABLE_ROW_CLICK = createAction('JOB_TABLE_ROW_CLICK');
 export const MODIFY_MARK_JOB_FOR_DELETE = createAction('MODIFY_MARK_JOB_FOR_DELETE');
 export const UPDATE_NOTIFICATIONAVAILABLE = createAction('UPDATE_NOTIFICATIONAVAILABLE');
-export const RELOAD_WEB_PAGE = createAction('RELOAD_WEB_PAGE');
 
 export const UPDATE_LOCALE = createAction('UPDATE_LOCALE');
-
-export const REST_API_FAILURE_UNAUTHORIZED = createAction('REST_API_FAILURE_UNAUTHORIZED');
-export const REST_API_FAILURE_FORBIDDEN = createAction('REST_API_FAILURE_FORBIDDEN');
