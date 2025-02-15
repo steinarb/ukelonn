fe80ab2334d8624aa096e2c7d01d6540d16281fa
Steinar Bang <sb@dod.no>
2025-02-15 15:30:55 +0100

    Set title and message correctly for notifications

--- ukelonn.web.frontend/src/main/frontend/components/Notification.js
+++ ukelonn.web.frontend/src/main/frontend/components/Notification.js
@@ -4,20 +4,20 @@ import { useGetLoginQuery, useGetNotificationQuery, api } from '../api';
 
 export default function Notification() {
     const { data: loginResponse = {}, isSuccess: loginIsSuccess } = useGetLoginQuery();
-    const { data: notification = [] } = useGetNotificationQuery(loginResponse.username, {
+    const { data: notifications, isSuccess: notificationIsSuccess } = useGetNotificationQuery(loginResponse.username, {
         skip: !loginIsSuccess,
         pollingInterval: 60000,
     });
     const dispatch = useDispatch();
 
-    if (notification.length) {
-        const  notificationMessage = notification[0].message;
+    if (notificationIsSuccess && notifications.length) {
+        const notification = notifications[0];
 
-        if (notificationMessage) {
+        if (notification.message) {
             // Update the balance after payment
             dispatch(api.endpoints.getAccount.initiate(loginResponse.username));
             if (Notification) {
-                spawnNotification(notificationMessage);
+                spawnNotification(notification);
             } else {
                 console.log('Notification not supported by browser');
             }
